1c2c3cac2df9e9975d63233b3363e4e94352faa4
HeejiSohn <96990839+HeejiSohn@users.noreply.github.com>
2022-05-28 13:57:55 +0900

    Initial Commit

---
+++ RandomWord/RandomWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomWordApp.swift
+//  RandomWord
+//
+//  Created by Heeji Sohn on 2022/05/28.
+//
+
+import SwiftUI
+
+@main
+struct RandomWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
