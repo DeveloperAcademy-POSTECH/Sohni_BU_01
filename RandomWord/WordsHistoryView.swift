10459160b4709c8bd8331da908efac712032d100
HeejiSohn <96990839+HeejiSohn@users.noreply.github.com>
2022-06-28 17:33:24 +0900

    Add TabView

---
+++ RandomWord/WordsHistoryView.swift
@@ -0,0 +1,20 @@
+//
+//  WordsHistoryView.swift
+//  RandomWord
+//
+//  Created by Heeji Sohn on 2022/06/28.
+//
+
+import SwiftUI
+
+struct WordsHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WordsHistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        WordsHistoryView()
+    }
+}
